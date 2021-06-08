501e4aedd69abd88abbcc47e8ce33847f40a7a6b
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2021-06-08 14:00:37 -0400

    Finished

--- index.js
+++ index.js
@@ -17,3 +17,40 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+/*************** Contact Form ***************/
+
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+
+    fetch("https://formsubmit.co/ajax/correamillancarlos@gmail.com",
+      {
+        method: "POST",
+        body: new FormData(e.target)
+      }
+    )
+    .then(res => res.ok ? res.json() : Promise.reject(res))
+    .then(json => {
+      console.log(json);
+      location.hash = "#contact-sent";
+      $form.reset();
+    })
+    .catch(err => {
+      console.log(err);
+      let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+      $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+    })
+    .finally(() => {
+      $loader.classList.add("none");
+      setTimeout(() => {
+        location.hash = "#close";
+      },3000);
+    });
+  });
+})(document);
